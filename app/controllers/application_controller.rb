a31616d8617a1bdc92127989c66a08c86335e714
Ravisankar-S <sravisankar11@gmail.com>
2026-05-17 08:13:56 +0000

    auth setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  
   before_action :configure_permitted_parameters,
                 if: :devise_controller?
 
@@ -20,5 +22,9 @@ class ApplicationController < ActionController::Base
         :preferred_locale
       ]
     )
+
+    def after_sign_in_path_for(resource)
+  dashboard_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get "dashboard/index"
   get "pages/landing"
   root "pages#landing"
 
   devise_for :users
+
+  get "/dashboard", to: "dashboard#index"
 end
